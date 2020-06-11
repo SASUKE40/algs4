cf72c496fed724c71c18dca35a8c742f236b38fe
Edward Elric <SASUKE688848@gmail.com>
2020-06-11 23:41:43 +0800

    feat: add BST

---
+++ src/BST.js
@@ -0,0 +1,76 @@
+class BST {
+  root = null;
+  get(key) {
+    let x = this.root;
+    while (x !== null) {
+      let cmp = key.compareTo(x.key);
+      if (cmp < 0) x = x.left;
+      else if (cmp > 0) x = x.right;
+      else return x.val;
+    }
+    return null;
+  }
+  put(key, val) {
+    this.root = this._put(this.root, key, val);
+  }
+  _put(x, key, val) {
+    if (x === null) return new Node(key, val, 1);
+    let cmp = key.compareTo(x.key);
+    console.log(key, x.key, cmp);
+
+    if (cmp < 0) x.left = this._put(x.left, key, val);
+    else if (cmp > 0) x.right = this._put(x.right, key, val);
+    else x.val = val;
+    x.count = 1 + this._size(x.left) + this._size(x.right);
+    return x;
+  }
+  floor(key) {
+    let x = this._floor(this.root, key);
+    if (x === null) return null;
+    return x.key;
+  }
+  _floor(x, key) {
+    if (x === null) return null;
+    let cmp = key.compareTo(x.key);
+    if (cmp === 0) return x;
+    if (cmp < 0) return this._floor(x.left, key);
+    let t = this._floor(x.right, key);
+    if (t !== null) return t;
+    else return x;
+  }
+  size() {
+    return this._size(this.root);
+  }
+  _size(x) {
+    if (x === null) return 0;
+    return x.count;
+  }
+}
+class Node {
+  left = null;
+  right = null;
+  key = null;
+  val = null;
+  count = 0;
+  constructor(key, val, count) {
+    this.key = key;
+    this.val = val;
+    this.count = count;
+  }
+}
+String.prototype.compareTo = function (that) {
+  return this < that ? -1 : this > that ? 1 : 0;
+};
+
+let bst = new BST();
+bst.put('A', 8);
+bst.put('C', 4);
+bst.put('E', 12);
+bst.put('H', 5);
+bst.put('L', 11);
+bst.put('M', 9);
+bst.put('P', 10);
+bst.put('R', 3);
+bst.put('S', 0);
+bst.put('X', 7);
+// console.log(JSON.stringify(bst, null, 2));
